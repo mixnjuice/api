5abecbe438e9659bd3a9de7b24fecd50758d2c6e
ayan4m1 <andrew@bulletlogic.com>
2019-05-20 23:38:31 -0400

    remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: './coverage',
   coverageReporters: ['html', 'text-summary'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
   testMatch: ['<rootDir>/src/**/*.test.js'],
   modulePaths: ['<rootDir>/src/']
 };

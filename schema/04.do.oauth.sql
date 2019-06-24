c02e7cec203f9ac600ebd88ae40f94bfcf1ca8a8
ayan4m1 <andrew@bulletlogic.com>
2019-06-23 20:41:51 -0400

    add new role and users_roles tables

--- schema/04.do.oauth.sql
+++ schema/04.do.oauth.sql
@@ -7,3 +7,24 @@ create table user_token (
   constraint pk_user_token primary key (token),
   constraint fk1_user_token foreign key (user_id) references "user" (id)
 );
+
+create table "role" (
+  id serial not null,
+  name varchar(200) not null,
+
+  constraint pk_role primary key (id)
+);
+
+insert into "role" (name) values ('Administrator');
+insert into "role" (name) values ('User');
+
+create table users_roles (
+  user_id bigint not null,
+  role_id int not null,
+  active boolean not null default true,
+  created timestamp not null default now(),
+
+  constraint pk_users_roles primary key (user_id, role_id),
+  constraint fk1_users_roles foreign key (user_id) references "user" (id),
+  constraint fk2_users_roles foreign key (role_id) references "role" (id)
+);

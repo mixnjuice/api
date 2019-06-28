c90eb8c2cc997da4bb2bd605a27f8b62f4c8ed4b
ayan4m1 <andrew@bulletlogic.com>
2019-06-28 09:33:28 -0400

    add error handling to email code and jsdoc

--- src/modules/email.js
+++ src/modules/email.js
@@ -1,10 +1,19 @@
 import AWS from 'aws-sdk';
 
+import loggers from './logging';
+
+const log = loggers('email');
+
 export default class Email {
-  constructor(options) {
-    AWS.config.update({ region: options.region });
+  /**
+   * Initialize the email sender usign a given SES region and from address.
+   *
+   * @param {Object} options Object containing region and fromAddress properties
+   */
+  constructor({ region, fromAddress }) {
+    AWS.config.update({ region });
     this.api = new AWS.SES({ apiVersion: '2010-12-01' });
-    this.fromAddress = options.fromAddress;
+    this.fromAddress = fromAddress;
 
     this.createActivationMessage = this.createActivationMessage.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
@@ -32,8 +41,12 @@ export default class Email {
   }
 
   async sendMessage(message) {
-    const send = this.api.sendEmail(message).promise();
+    try {
+      const send = this.api.sendEmail(message).promise();
 
-    return await send;
+      return await send;
+    } catch (error) {
+      log.error(error);
+    }
   }
 }

a19310c6033527853553825823444e399f16d2f9
ayan4m1 <andrew@bulletlogic.com>
2019-06-28 21:35:11 -0400

    WIP rework of model loading to support inline associations

--- src/models/user.js
+++ src/models/user.js
@@ -1,34 +1,37 @@
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      id: {
-        type: DataTypes.BIGINT,
-        allowNull: false,
-        primaryKey: true
-      },
-      emailAddress: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      created: {
-        type: DataTypes.DATE,
-        allowNull: false
-      },
-      activationCode: {
-        type: DataTypes.STRING
-      }
-    },
-    {
-      tableName: 'user',
-      createdAt: 'created',
-      updatedAt: false
-    }
-  );
+import { Model, DataTypes } from 'sequelize';
 
-  return User;
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          primaryKey: true
+        },
+        emailAddress: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        created: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        activationCode: {
+          type: DataTypes.STRING
+        }
+      },
+      {
+        sequelize,
+        underscored: true,
+        tableName: 'user',
+        createdAt: 'created',
+        updatedAt: false
+      }
+    );
+  }
 };

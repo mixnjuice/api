2da825904f308ba6880618cdacdaf4299f09b32d
ayan4m1 <andrew@bulletlogic.com>
2019-07-02 00:57:52 -0400

    fix URL generation and email sending in register route, add activation code handler route

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
-import { check, validationResult } from 'express-validator/check';
+import { check, query, validationResult } from 'express-validator/check';
 
 import Email from '../modules/email';
-import models from '../modules/database';
-import loggers from '../modules/logging';
+import models, { handleError } from '../modules/database';
+import logging from '../modules/logging';
 import configs from '../modules/config';
-import { hashPassword, generateToken } from '../modules/auth';
+import { hashPassword, generateToken, buildWebUrl } from '../modules/util';
 
 const router = Router();
-const log = loggers('register');
+const log = logging('register');
 const { User, UserProfile } = models;
-const { email: emailConfig, web: webConfig } = configs;
+const { email: emailConfig, api: apiConfig } = configs;
+const {
+  tokens: { length: tokenLength }
+} = apiConfig;
 const emailer = new Email(emailConfig);
 
 router.post(
@@ -61,25 +64,52 @@ router.post(
       });
 
       // build activation URL
-      const { hostname, port } = webConfig;
+      const url = buildWebUrl(`/register/activate?code=${activationCode}`);
+      const message = emailer.createActivationEmail(emailAddress, url);
 
-      let url = `https://${hostname}`;
-
-      if (port !== 443) {
-        url += `:${port}`;
-      }
-
-      url += `/register/activate?code=${activationCode}`;
-
-      await emailer.sendActivationEmail(emailAddress, url);
-
+      emailer.sendMessage(message);
       res.status(200).end();
     } catch (error) {
-      log.error(error.message);
-      log.error(error.stack);
-      res.status(500).end();
+      handleError(error, res);
     }
   }
 );
+
+router.get('/activate', [
+  query('code')
+    .isLength({ min: tokenLength, max: tokenLength })
+    .withMessage('length')
+    .matches(/[A-Za-z0-9_-]+/)
+    .withMessage('format'),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { code: activationCode } = req.query;
+
+    try {
+      const result = await User.findAll({
+        where: {
+          activationCode
+        }
+      });
+
+      if (!Array.isArray(result) || result.length === 0) {
+        return res.status(400).end();
+      }
+
+      const [user] = result;
+
+      user.set('activationCode', null);
+      await user.save();
+      res.redirect(buildWebUrl('/login'));
+    } catch (error) {
+      handleError(error, res);
+    }
+  }
+]);
 
 export default router;

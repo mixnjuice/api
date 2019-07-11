7eca2e02b743c1b5391e2d99fb0341941bd87a1c
daviddyess <david.dyess@gmail.com>
2019-07-11 15:37:39 -0600

    Added UserProfile model

---
+++ src/models/userProfile.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserProfile = sequelize.define(
+    'UserProfile',
+    {
+      userId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: sequelize.User,
+          key: 'id'
+        }
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      bio: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNul: true
+      }
+    },
+    {
+      sequelize,
+      underscored: true,
+      tableName: 'user_profile',
+      createdAt: false,
+      updatedAt: false
+    }
+  );
+
+  UserProfile.associate = function(models) {
+    this.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+
+  return UserProfile;
+};

f294bb406a4c92c43a5dda5b6dd8eda5da50a268
ayan4m1 <andrew@bulletlogic.com>
2019-07-13 04:46:07 -0400

    add passport-wrapping middleware to auth.js, use it in flavor.js

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -4,6 +4,7 @@ import nanoid from 'nanoid';
 import passport from 'passport';
 import oauth2orize from 'oauth2orize';
 import BearerStrategy from 'passport-http-bearer';
+import AnonymousStrategy from 'passport-anonymous';
 
 import configs from './config';
 import models from './database';
@@ -13,7 +14,11 @@ const log = loggers('auth');
 const { UserToken, User } = models;
 
 const { web: webConfig } = configs;
-const { age: tokenAge, length: tokenLength } = webConfig.tokens;
+const {
+  age: tokenAge,
+  length: tokenLength,
+  validate: validateTokens
+} = webConfig.tokens;
 
 const generateToken = () => {
   return nanoid(tokenLength);
@@ -21,6 +26,42 @@ const generateToken = () => {
 
 const compareHashAndPassword = async (hash, password) => {
   return await bcrypt.compare(password, hash);
+};
+
+const authorize = async (token, done) => {
+  try {
+    if (!token.trim()) {
+      return done(
+        new oauth2orize.TokenError('Missing token!', 'invalid_request')
+      );
+    }
+
+    const result = await UserToken.findAll({
+      where: {
+        token
+      },
+      include: [
+        {
+          model: User,
+          required: true
+        }
+      ]
+    });
+
+    if (!Array.isArray(result) || result.length === 0) {
+      return done(
+        new oauth2orize.TokenError('Authentication failed.', 'invalid_grant')
+      );
+    }
+
+    const user = result.shift().User;
+
+    done(null, user, { scope: 'all' });
+  } catch (error) {
+    log.error(error.message);
+    log.error(error.stack);
+    done(new oauth2orize.OAuth2Error(error));
+  }
 };
 
 const authServer = oauth2orize.createServer();
@@ -85,52 +126,21 @@ authServer.exchange(
   })
 );
 
+/**
+ * Provide a wrapper around passport.authenticate with the appropriate strategies selected.
+ */
+export const authenticate = () => [
+  passport.initialize(),
+  passport.authenticate(validateTokens ? 'bearer' : ['bearer', 'anonymous'], {
+    session: false
+  })
+];
+
 export default app => {
   passport.use(
-    new BearerStrategy(async (token, done) => {
-      try {
-        if (!token.trim()) {
-          return done(
-            new oauth2orize.TokenError('Missing token!', 'invalid_request')
-          );
-        }
-
-        const result = await UserToken.findAll({
-          where: {
-            token
-          },
-          include: [
-            {
-              model: User,
-              required: true
-            }
-          ]
-        });
-
-        if (!Array.isArray(result) || result.length === 0) {
-          return done(
-            new oauth2orize.TokenError(
-              'Authentication failed.',
-              'invalid_grant'
-            )
-          );
-        }
-
-        const user = result.shift().User;
-
-        done(null, user, { scope: 'all' });
-      } catch (error) {
-        log.error(error.message);
-        log.error(error.stack);
-        done(new oauth2orize.OAuth2Error(error));
-      }
-    })
+    validateTokens ? new BearerStrategy(authorize) : new AnonymousStrategy()
   );
 
   // allow requests to obtain a token
   app.post('/oauth/token', authServer.token(), authServer.errorHandler());
-
-  // put all API routes behind bearer token auth
-  app.use('/api/*', passport.initialize());
-  app.use('/api/*', passport.authenticate('bearer', { session: false }));
 };

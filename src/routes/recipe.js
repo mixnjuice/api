07dad15fb0ca40feed19142770a2ca7fab930c62
daviddyess <david.dyess@gmail.com>
2019-07-14 01:23:46 -0600

    Added recipe route, recipe.test

---
+++ src/routes/recipe.js
@@ -0,0 +1,130 @@
+import { Router } from 'express';
+import { body, param, validationResult } from 'express-validator';
+
+import { authenticate } from '../modules/auth';
+import models from '../modules/database';
+import loggers from '../modules/logging';
+
+const router = Router();
+const log = loggers('recipe');
+const { Recipe, UserProfile, RecipesFlavors, RecipesDiluents } = models;
+
+/**
+ * GET Recipe
+ * @param id int
+ */
+router.get(
+  '/:id',
+  authenticate(),
+  [
+    param('id')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request for ${req.params.id}`);
+    try {
+      const result = await Recipe.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [
+          {
+            model: UserProfile,
+            required: true
+          },
+          {
+            model: RecipesFlavors,
+            required: true
+          },
+          {
+            model: RecipesDiluents,
+            required: true
+          }
+        ]
+      });
+
+      if (!Array.isArray(result) || result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result.shift().shift());
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * POST Create a Recipe and associations
+ */
+router.post(
+  '/',
+  authenticate(),
+  [
+    body('userid')
+      .isNumeric()
+      .toInt(),
+    body('name')
+      .isString()
+      .isLength({ min: 1 })
+      .withMessage('length'),
+    body('flavors')
+      .isArray()
+      .withMessage('array'),
+    body('diluents')
+      .isArray()
+      .withMessage('array')
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request for NEW RECIPE`);
+    try {
+      const result = await Recipe.create({
+        userId: req.body.userid,
+        name: req.body.name,
+        viewCount: 0,
+        RecipesFlavors: req.body.flavors, // Array of flavors
+        RecipesDiluents: req.body.diluents, // Array of diluents
+        include: [
+          {
+            model: RecipesFlavors,
+            required: true
+          },
+          {
+            model: RecipesDiluents,
+            required: true
+          }
+        ]
+      });
+
+      if (result[0].length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result.shift().shift());
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * PUT Update Recipe
+ */
+
+export default router;
---
+++ src/routes/recipe.test.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import request from 'supertest';
+import passport from 'passport';
+import AnonymousStrategy from 'passport-anonymous';
+
+import recipe from './recipe';
+import database from '../modules/database';
+
+/* eslint-disable camelcase */
+describe('recipe resource', () => {
+  const app = express();
+
+  passport.use(new AnonymousStrategy());
+  app.use(recipe);
+
+  afterAll(() => {
+    database.sequelize.close();
+  });
+  /*
+  it('returns valid vendors', done => {
+    request(app)
+      .get('/1')
+      .expect(
+        200,
+        {
+          // Recipe Data
+        },
+        done
+      );
+  });
+*/
+  it('returns 204 for missing recipe', done => {
+    request(app)
+      .get('/99999999999999999999')
+      .expect(204, done);
+  });
+
+  it('returns 400 for invalid recipe', done => {
+    request(app)
+      .get('/0')
+      .expect(400, done);
+  });
+
+  it('returns 400 for invalid recipe', done => {
+    request(app)
+      .get('/ham')
+      .expect(400, done);
+  });
+});

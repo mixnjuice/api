42ae51ad4cb5ca8b489b1da5686183f9cd07bd69
daviddyess <david.dyess@gmail.com>
2019-07-14 01:25:14 -0600

    Added vendor.test

---
+++ src/routes/vendor.test.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import request from 'supertest';
+import passport from 'passport';
+import AnonymousStrategy from 'passport-anonymous';
+
+import vendor from './vendor';
+import database from '../modules/database';
+
+/* eslint-disable camelcase */
+describe('vendor resource', () => {
+  const app = express();
+
+  passport.use(new AnonymousStrategy());
+  app.use(vendor);
+
+  afterAll(() => {
+    database.sequelize.close();
+  });
+
+  it('returns valid vendor', done => {
+    request(app)
+      .get('/1')
+      .expect(
+        200,
+        { id: 1, name: 'Baker Flavors', slug: 'baker-flavors', code: 'BF' },
+        done
+      );
+  });
+
+  it('returns 204 for missing vendor', done => {
+    request(app)
+      .get('/20000')
+      .expect(204, done);
+  });
+
+  it('returns 400 for invalid vendor', done => {
+    request(app)
+      .get('/0')
+      .expect(400, done);
+  });
+
+  it('returns 400 for invalid vendor', done => {
+    request(app)
+      .get('/ham')
+      .expect(400, done);
+  });
+});

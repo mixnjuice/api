2ca06853d5c957bb72c679efd5e9c39925b31d72
daviddyess <david.dyess@gmail.com>
2019-07-14 16:35:20 -0600

    Updated user route - made routes more specific, to prevent unexpected matches in the future; added GET for a single flavor in the user's flavor stash

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,7 +14,7 @@ const { Flavor, Recipe, User, UsersFlavors, UserProfile, Vendor } = models;
  * @param userid int
  */
 router.get(
-  '/:userid',
+  '/:userid(\\d+)',
   authenticate(),
   [
     param('userid')
@@ -56,7 +56,7 @@ router.get(
  * @param userid int
  */
 router.get(
-  '/:userid/profile',
+  '/:userid(\\d+)/profile',
   authenticate(),
   [
     param('userid')
@@ -95,7 +95,7 @@ router.get(
  * @param userid int
  */
 router.put(
-  '/:userid/profile',
+  '/:userid(\\d+)/profile',
   authenticate(),
   [
     param('userid')
@@ -141,7 +141,7 @@ router.put(
  * @param userid int
  */
 router.get(
-  '/:userid/recipes',
+  '/:userid(\\d+)/recipes',
   authenticate(),
   [
     param('userid')
@@ -181,7 +181,7 @@ router.get(
  * @param userid int
  */
 router.get(
-  '/:userid/flavors',
+  '/:userid(\\d+)/flavors',
   authenticate(),
   [
     param('userid')
@@ -228,11 +228,67 @@ router.get(
   }
 );
 /**
+ * GET A User Flavor
+ * @param userid int
+ * @param flavorid int
+ */
+router.get(
+  '/:userid(\\d+)/flavor/:flavorid(\\d+)',
+  authenticate(),
+  [
+    param('userid')
+      .isNumeric()
+      .toInt(),
+    param('flavorid')
+      .isNumeric()
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request flavor stash for user ${req.params.userid}`);
+    try {
+      const result = await UsersFlavors.findAll({
+        where: {
+          userId: req.params.userid,
+          flavorId: req.params.flavorid
+        },
+        include: [
+          {
+            model: Flavor,
+            required: true,
+            include: [
+              {
+                model: Vendor,
+                required: true
+              }
+            ]
+          }
+        ]
+      });
+
+      if (result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
  * POST Add Flavor to User's Flavor Stash
  * @param userid int - User ID
  */
 router.post(
-  '/:userid/flavor',
+  '/:userid(\\d+)/flavor',
   authenticate(),
   [
     param('userid')
@@ -272,7 +328,7 @@ router.post(
  * PUT Update User's Flavor Stash Entry
  */
 router.put(
-  '/:userid/flavor/:flavorid',
+  '/:userid(\\d+)/flavor/:flavorid(\\d+)',
   authenticate(),
   [
     param('userid')
@@ -321,7 +377,7 @@ router.put(
  * DELETE Remove User's Flavor Stash Entry
  */
 router.delete(
-  '/:userid/flavor/:flavorid',
+  '/:userid(\\d+)/flavor/:flavorid(\\d+)',
   authenticate(),
   [
     param('userid')

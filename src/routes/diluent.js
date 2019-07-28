7b0054ce4d08fc4eec1f82555818fdb2413c526c
daviddyess <david.dyess@gmail.com>
2019-07-27 21:50:07 -0600

    Added PUT and DELETE requests to /diluent route

--- src/routes/diluent.js
+++ src/routes/diluent.js
@@ -71,7 +71,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    log.info(`request for NEW DILUENT`);
+    log.info(`request for new diluent`);
     try {
       const result = await Diluent.create({
         name: req.body.name,
@@ -93,7 +93,102 @@ router.post(
   }
 );
 /**
- * PUT Update Recipe
+ * PUT Update a Diluent
+ * @param id int
+ * @body name string
+ * @body slug string
+ * @body code string
+ * @body density numeric
  */
+router.put(
+  '/:id',
+  authenticate(),
+  [
+    param('id')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    body('name')
+      .isString()
+      .isLength({ min: 1 })
+      .withMessage('length'),
+    body('slug').isString(),
+    body('code').isString(),
+    body('density').isNumeric()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request to update diluent ${req.params.id}`);
+    try {
+      const result = await Diluent.update(
+        {
+          name: req.body.name,
+          slug: req.body.slug,
+          code: req.body.code,
+          density: req.body.density
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      );
+
+      if (result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * Delete Diluent
+ * @param id int
+ */
+router.delete(
+  '/:id',
+  authenticate(),
+  [
+    param('id')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request to delete diluent id ${req.params.id}`);
+    try {
+      const result = await Diluent.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      if (!result || result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
 export default router;

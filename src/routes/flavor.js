907f1fa32a6c5e4695f990cd8fd0beecdd229694
daviddyess <david.dyess@gmail.com>
2019-07-29 17:11:38 -0600

    Updated /flavor route to support flavor data identifiers

--- src/routes/flavor.js
+++ src/routes/flavor.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { param, validationResult } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
 
 import { authenticate } from '../modules/auth';
 import models from '../modules/database';
@@ -7,8 +7,12 @@ import loggers from '../modules/logging';
 
 const router = Router();
 const log = loggers('flavor');
-const { Flavor, Vendor } = models;
+const { DataSupplier, Flavor, FlavorIdentifier, Vendor } = models;
 
+/**
+ * GET Flavor by ID
+ * @param id int
+ */
 router.get(
   '/:id',
   authenticate(),
@@ -27,19 +31,6 @@ router.get(
 
     log.info(`request for ${req.params.id}`);
     try {
-      /* const result = await database.sequelize.query(
-        `select
-          v.code vendor_code,
-          v.name vendor_name,
-          f.name flavor_name
-        from
-          flavor f
-          join vendor v
-            on f.vendor_id = v.id
-        where
-          f.id = $1::bigint`,
-        { bind: [req.params.id] }
-      );*/
       const result = await Flavor.findOne({
         where: {
           id: req.params.id
@@ -50,6 +41,264 @@ router.get(
             require: true
           }
         ]
+      });
+
+      if (!result || result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * GET Flavor Data Supplier Identifiers
+ * @param id int
+ */
+router.get(
+  '/:id/identifiers',
+  authenticate(),
+  [
+    param('id')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(`request for flavor id ${req.params.id} identifiers`);
+    try {
+      const result = await FlavorIdentifier.findAll({
+        where: {
+          flavorId: req.params.id
+        },
+        include: [
+          {
+            model: DataSupplier,
+            require: true
+          }
+        ]
+      });
+
+      if (!result || result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * GET Flavor Data Supplier Identifier
+ * @param flavorId int
+ * @param dataSupplierId int
+ */
+router.get(
+  '/:flavorId/identifier/:dataSupplierId',
+  authenticate(),
+  [
+    param('flavorId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    param('dataSupplierId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(
+      `request for flavor id ${req.params.flavorId} data supplier id ${req.params.dataSupplierId} identifer`
+    );
+    try {
+      const result = await FlavorIdentifier.findOne({
+        where: {
+          flavorId: req.params.flavorId,
+          dataSupplierId: req.params.dataSupplierId
+        },
+        include: [
+          {
+            model: DataSupplier,
+            require: true
+          }
+        ]
+      });
+
+      if (!result || result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * POST Create a Flavor Identifier
+ * @param id int
+ * @param dataSupplierId int
+ * @body identifier string
+ */
+router.post(
+  '/:id/identifier',
+  authenticate(),
+  [
+    param('id')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    body('dataSupplierId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    body('identifier')
+      .isString()
+      .isLength({ min: 1 })
+      .withMessage('length')
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(
+      `request for new flavor identifier for flavor id ${req.params.id}`
+    );
+    try {
+      const result = await FlavorIdentifier.create({
+        flavorId: req.params.id,
+        dataSupplierId: req.body.dataSupplierId,
+        identifier: req.body.identifier
+      });
+
+      if (result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * PUT Update a Flavor Identifier
+ * @param flavorId int
+ * @param dataSupplierId int
+ * @body identifier string
+ */
+router.put(
+  '/:flavorId/identifier/:dataSupplierId',
+  authenticate(),
+  [
+    param('flavorId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    param('dataSupplierId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    body('identifier')
+      .isString()
+      .isLength({ min: 1 })
+      .withMessage('length')
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(
+      `request to update flavor id ${req.params.flavorId} identifier id ${req.params.dataSupplierId}`
+    );
+    try {
+      const result = await FlavorIdentifier.update(
+        {
+          identifier: req.body.identifier
+        },
+        {
+          where: {
+            flavorId: req.params.flavorId,
+            dataSupplierId: req.params.dataSupplierId
+          }
+        }
+      );
+
+      if (result.length === 0) {
+        return res.status(204).end();
+      }
+
+      res.type('application/json');
+      res.json(result);
+    } catch (error) {
+      log.error(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+/**
+ * Delete Diluent
+ * @param flavorId int
+ * @param dataSupplierId int
+ */
+router.delete(
+  '/:flavorId/identifier/:dataSupplierId',
+  authenticate(),
+  [
+    param('flavorId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt(),
+    param('dataSupplierId')
+      .isNumeric()
+      .isInt({ min: 1 })
+      .toInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    log.info(
+      `request to delete flavor id ${req.params.flavorId} identifier id ${req.params.dataSupplierId}`
+    );
+    try {
+      const result = await FlavorIdentifier.destroy({
+        where: {
+          flavorId: req.params.flavorId,
+          dataSupplierId: req.params.dataSupplierId
+        }
       });
 
       if (!result || result.length === 0) {
--- src/routes/flavor.test.js
+++ src/routes/flavor.test.js
@@ -2,6 +2,7 @@ import express from 'express';
 import request from 'supertest';
 import passport from 'passport';
 import AnonymousStrategy from 'passport-anonymous';
+import bodyParser from 'body-parser';
 
 import flavor from './flavor';
 import database from '../modules/database';
@@ -11,15 +12,50 @@ describe('flavor route resource', () => {
   const app = express();
 
   passport.use(new AnonymousStrategy());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(flavor);
 
   afterAll(() => {
     database.sequelize.close();
   });
 
-  it('returns valid flavor', done => {
+  it('GET returns valid flavor', done => {
     request(app)
       .get('/123')
+      .expect(200, done);
+  });
+
+  it('GET returns valid flavor identifiers', done => {
+    request(app)
+      .get('/1/identifiers')
+      .expect(200, done);
+  });
+
+  it('GET returns valid flavor identifier', done => {
+    request(app)
+      .get('/1/identifier/1')
+      .expect(200, done);
+  });
+
+  it('POST creates valid flavor identifier', done => {
+    request(app)
+      .post('/1/identifier')
+      .send({ dataSupplierId: 1, identifier: 'cap_27-bears' })
+      .expect(200, done);
+  });
+
+  it('PUT updates valid flavor identifier', done => {
+    request(app)
+      .put('/1/identifier/1')
+      .send({ identifier: 'cap_27-bears' })
+      .expect(200, done);
+  });
+
+  it('DELETE deletes flavor identifier', done => {
+    request(app)
+      .delete('/1/identifier/1')
+      .send({ identifier: 'cap_27-bears' })
       .expect(200, done);
   });
 

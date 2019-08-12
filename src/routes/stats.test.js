29ffe07165b4494cd6b4ba13e7f34cfd7af5ee3c
David Dyess II <david.dyess@gmail.com>
2019-08-11 23:58:04 -0600

    Added test for stats route

---
+++ src/routes/stats.test.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import request from 'supertest';
+import passport from 'passport';
+import AnonymousStrategy from 'passport-anonymous';
+
+import stats from './stats';
+import database from '../modules/database';
+
+/* eslint-disable camelcase */
+describe('stats route resource', () => {
+  const app = express();
+
+  passport.use(new AnonymousStrategy());
+  app.use(stats);
+
+  afterAll(() => {
+    database.sequelize.close();
+  });
+
+  it('returns valid stats', done => {
+    request(app)
+      .get('/dashboard')
+      .expect(200, done);
+  });
+
+  it('returns 404 for page not found', done => {
+    request(app)
+      .get('/error')
+      .expect(404, done);
+  });
+});

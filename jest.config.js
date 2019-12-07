418df7132bb2974757a2d82120abe212d4b38381
Andrew DeLisa <andrew@bulletlogic.com>
2019-12-07 15:08:06 -0500

    Add codecov support (#42)
    * enable lcov reporter
    
    * run codecov and export token to BASH_ENV
    
    * fix env var name and add context to build step
    
    * try invoking codecov a different way

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: './coverage',
-  coverageReporters: ['html', 'text-summary'],
+  coverageReporters: ['html', 'text-summary', 'lcov'],
   testMatch: ['<rootDir>/src/**/*.test.js'],
   modulePaths: ['<rootDir>/src/']
 };

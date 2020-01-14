1b6317b079018a308e84d77bfde80331a75ca067
Andrew DeLisa <andrew@bulletlogic.com>
2020-01-14 00:40:22 -0500

    add schema and models for flavor ingredient safety (#50)

---
+++ schema/18.do.flavor-safety.sql
@@ -0,0 +1,61 @@
+create table ingredient_category (
+  id serial not null,
+  name varchar(50) not null,
+  ordinal int not null,
+  description text not null,
+
+  constraint pk_ingredient_category primary key (id),
+  constraint uk1_ingredient_category unique (name),
+  constraint uk2_ingredient_category unique (ordinal)
+);
+
+insert into ingredient_category (name, ordinal, description) values (1, 1, 'Flavors in this category should not be used.');
+insert into ingredient_category (name, ordinal, description) values ('Caution', 2, 'Flavors in this category should only be used sparingly.');
+insert into ingredient_category (name, ordinal, description) values (3, 3, 'Flavors in this category are being researched further.');
+
+create table ingredient (
+  id serial not null,
+  name varchar(100) not null,
+  cas_number varchar(50) not null,
+  ingredient_category_id int not null,
+  notes text default null,
+  created timestamp default now(),
+  updated timestamp default null,
+
+  constraint pk_ingredient primary key (id),
+  constraint uk1_ingredient unique (name),
+  constraint uk2_ingredient unique (cas_number),
+  constraint fk1_ingredient foreign key (ingredient_category_id) references ingredient_category (id)
+);
+
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Acetoin', '513-86-0', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Acetyl Propionyl', '600-14-6', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Acetyl Pyrazine', '22047-25-2', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Caprylic Acid', '124-07-2', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Caramel color', '8028-89-5', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Cocoa Butter', '8002-31-1', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Corn Syrup', '8029-43-4', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Diacetyl', '431-03-8', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Fructose', '57-48-7', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Furfuryl alcohol', '98-0-0', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Molasses', '8052-35-5', 2);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Peppermint oil', '8006-90-4', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Polysorbate 80', '9005-65-6 ', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Rose oxide', '16409-43-1', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Sodium Chloride', '7647-14-5', 2);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Sucralose', '56038-13-2', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Triethyl Citrate', '77-93-0', 3);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Vitamin E Acetate', '58-95-7', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Vitamin E', '1406-18-4', 1);
+insert into ingredient (name, cas_number, ingredient_category_id) values ('Xylose', '58-86-6', 1);
+
+create table flavors_ingredients (
+  flavor_id bigint not null,
+  ingredient_id int not null,
+  created timestamp not null default now(),
+  updated timestamp not null,
+
+  constraint pk_flavors_ingredients primary key (flavor_id, ingredient_id),
+  constraint fk1_flavors_ingredients foreign key (flavor_id) references flavor (id),
+  constraint fk2_flavors_ingredients foreign key (ingredient_id) references ingredient (id)
+);

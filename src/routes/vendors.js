ba115b4f624d1345a207b4a12d3b227fdea3aa41
David Dyess II <david.dyess@gmail.com>
2020-02-14 11:07:27 -0700

    Added /vendors/count route (#59)
    * Added /vendors/count route
    
    * Respond with an object for /vendors/count

--- src/routes/vendors.js
+++ src/routes/vendors.js
@@ -56,5 +56,24 @@ router.get(
     }
   }
 );
+/**
+ * GET Vendor Stats
+ */
+router.get('/count', authenticate(), async (req, res) => {
+  log.info(`request for vendor stats`);
+  try {
+    const vendors = await Vendor.count();
+    // Results
+    const result = {
+      vendors
+    };
+
+    res.type('application/json');
+    res.json(result);
+  } catch (error) {
+    log.error(error.message);
+    res.status(500).send(error.message);
+  }
+});
 
 export default router;

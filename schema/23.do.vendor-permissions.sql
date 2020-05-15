bd3d4c82f9b9dc38de9ed849c610933cf94ffaf3
daviddyess <david.dyess@gmail.com>
2020-05-15 16:44:35 -0600

    Added `vendors` to migration 23

--- schema/23.do.vendor-permissions.sql
+++ schema/23.do.vendor-permissions.sql
@@ -1,10 +1,10 @@
-insert into permission_subject (name) values ('vendor');
+insert into permission_subject (name) values ('vendor'), ('vendors');
 
 insert into roles_permissions
   select r.id, ps.id, pa.id
   from
     "role" r
-    join permission_subject ps on ps.name in ('vendor')
+    join permission_subject ps on ps.name in ('vendor', 'vendors')
     join permission_action pa on pa.name = 'read'
   where r.name = 'Guest';
 
@@ -12,7 +12,7 @@ insert into roles_permissions
   select r.id, ps.id, pa.id
   from
     "role" r
-    join permission_subject ps on ps.name in ('vendor')
+    join permission_subject ps on ps.name in ('vendor', 'vendors')
     join permission_action pa on pa.name in ('read', 'create', 'update', 'delete')
   where r.name = 'User';
 
@@ -20,7 +20,7 @@ insert into roles_permissions
   select r.id, ps.id, pa.id
   from
     "role" r
-    join permission_subject ps on ps.name in ('vendor')
+    join permission_subject ps on ps.name in ('vendor', 'vendors')
     join permission_action pa on pa.name in ('read', 'create', 'update', 'delete', 'manage')
   where r.name = 'Administrator';
   
--- schema/23.undo.vendor-permissions.sql
+++ schema/23.undo.vendor-permissions.sql
@@ -2,3 +2,8 @@ delete from roles_permissions
   where permission_subject_id in (select id from permission_subject where name = 'vendor');
 
 delete from permission_subject where name = 'vendor';
+
+delete from roles_permissions
+  where permission_subject_id in (select id from permission_subject where name = 'vendors');
+
+delete from permission_subject where name = 'vendors';

52407a4c02188852006d3248ff253d739fe438c8
daviddyess <david.dyess@gmail.com>
2020-05-15 16:23:47 -0600

    Added a migration containing the `vendor` permission subject

---
+++ schema/23.do.vendor-permissions.sql
@@ -0,0 +1,26 @@
+insert into permission_subject (name) values ('vendor');
+
+insert into roles_permissions
+  select r.id, ps.id, pa.id
+  from
+    "role" r
+    join permission_subject ps on ps.name in ('vendor')
+    join permission_action pa on pa.name = 'read'
+  where r.name = 'Guest';
+
+insert into roles_permissions
+  select r.id, ps.id, pa.id
+  from
+    "role" r
+    join permission_subject ps on ps.name in ('vendor')
+    join permission_action pa on pa.name in ('read', 'create', 'update', 'delete')
+  where r.name = 'User';
+
+insert into roles_permissions
+  select r.id, ps.id, pa.id
+  from
+    "role" r
+    join permission_subject ps on ps.name in ('vendor')
+    join permission_action pa on pa.name in ('read', 'create', 'update', 'delete', 'manage')
+  where r.name = 'Administrator';
+  
---
+++ schema/23.undo.vendor-permissions.sql
@@ -0,0 +1,4 @@
+delete from roles_permissions
+  where permission_subject_id in (select id from permission_subject where name = 'vendor');
+
+delete from permission_subject where name = 'vendor';

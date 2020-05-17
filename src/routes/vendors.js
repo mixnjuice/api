ad20e87b05af209ce646a53bf13de9350401237b
daviddyess <david.dyess@gmail.com>
2020-05-16 19:36:24 -0600

    Use handleCount for vendors count route

--- src/routes/vendors.js
+++ src/routes/vendors.js
@@ -4,7 +4,11 @@ import { query } from 'express-validator';
 import { authenticate, ensurePermission } from 'modules/auth';
 import models from 'modules/database';
 import loggers from 'modules/logging';
-import { handleFindAll, handleValidationErrors } from 'modules/utils/request';
+import {
+  handleCount,
+  handleFindAll,
+  handleValidationErrors
+} from 'modules/utils/request';
 
 const router = Router();
 const log = loggers('vendors');
@@ -40,22 +44,7 @@ router.get(
   '/count',
   authenticate(),
   ensurePermission('vendors', 'read'),
-  async (req, res) => {
-    log.info(`request for vendor stats`);
-    try {
-      const vendors = await Vendor.count();
-      // Results
-      const result = {
-        vendors
-      };
-
-      res.type('application/json');
-      res.json(result);
-    } catch (error) {
-      log.error(error.message);
-      res.status(500).send(error.message);
-    }
-  }
+  handleCount(Vendor)
 );
 
 export default router;
